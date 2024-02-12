e0e1388f51c5bdd92a354128eb78da364c9003d9
brunogoniadis <brunogoniadislima@gmail.com>
2024-02-11 21:59:08 -0300

    feat/fix: account withdraw

--- src/index.js
+++ src/index.js
@@ -23,6 +23,17 @@ function verifyIdExistsAccountCPF(request, response, next) {
 
 }
 
+function getBalance(statement) {
+    const balance = statement.reduce(
+        (acc, operation) => {
+            if (operation.type === 'credit') {
+                return acc + operation.amount
+            } else {
+                return acc - operation.amount
+            }
+        }, 0);
+    return balance
+}
 
 app.post("/account", (request, response) => {
     const { cpf, name } = request.body;
@@ -70,6 +81,31 @@ app.post("/deposit", verifyIdExistsAccountCPF, (request, response) => {
 
     return response.status(201).send()
 })
+
+app.post("/withdraw", verifyIdExistsAccountCPF, (request, response) => {
+    const { amount } = request.body;
+    const { customer } = request;
+    const balance = getBalance(customer.statement);
+
+    console.log('balance', balance)
+
+    console.log('amount', amount)
+
+
+    if (balance < amount) {
+        return response.status(400).json({ error: "Insufficient funds!" });
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    };
+
+    customer.statement.push(statementOperation);
+
+    return response.status(201).send();
+});
 
 app.listen(3333)
 
